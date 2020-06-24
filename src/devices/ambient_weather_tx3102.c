dbabfee4d3738f131c6dc438fdf80ed10f11fd8c
sycophantic <danielgrinkevich@gmail.com>
2020-06-24 18:54:59 -0400

    Got data for a TX-3110, rewrote a lot

--- src/devices/ambient_weather_tx3102.c
+++ src/devices/ambient_weather_tx3102.c
@@ -18,20 +18,22 @@ A transmission starts with a preamble of 0xAAAAAAAAAA and codeword 0x2DD4
 
 Data layout:
 
-    01 02 03 04 05 06 07 09 10 11 12 13 14 15 16 17 18 19
-    II II ?? ?? ?? ?? ?C ?? ?? ?? ?? ?? TT TB MM ?? ?? SS	
-    1a 92 18 70 0c 31 41 ff ff ff 00 00 23 42 01 ff f0 a7
-    c5 f9 18 70 08 22 42 ff ff ff 00 00 26 82 01 ff f0 76
+        00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17
+        II II FF DD ?? ?? ?C ?? ?? ?? ?? ?? TT TB MM ?? ?? SS	
+TX-3102 1a 92 18 70 0c 31 41 ff ff ff 00 00 23 42 01 ff f0 a7
+TX-3102 c5 f9 18 70 08 22 42 ff ff ff 00 00 26 82 01 ff f0 76
+TX-3110 9c d3 18 30 15 32 27 00 00 00 00 00 27 26 39 00 00 c3
 
-- I: CRC-16/X-MODEM (bytes 03 to 18)
+- I: CRC-16/X-MODEM (bytes 02 to 16)
+- F: Probably a family code?
+- D: Device type?
 - C: Channel (1-7)
 - T: 12 bit temperature, 234 = 23.4C (byte 13 might be used, couldn't get the probe below -9C)
 - B: Battery status and sign for temperature (S?B?), ex: 0010 good voltage, 0000 voltage below 2.6V, 1010 negative temperature and good voltage, 1000 negative temperature and low voltage 
-- M: 8 bit soil moisture, (values 01 - 16)
-- S: SUM-8 of bytes 03 to 19 XOR with FF
+- M: 8 bit soil moisture, (values 01 - 16 for TX-3102 or 00 to 99 for TX-3110)
+- S: SUM-8 of bytes 02 to 16 XOR with FF
 
-Bytes 09 to 11 and 17 to 18 are probably for temperature/humidity/spa sensors.
-Bytes 03 to 07 are probably the device type and unique ID, but the data I have is only for two soil probes.
+Bytes 02 to 06 are probably the family, device type, and unique ID.
 
 From the manufacturer, "The WS-8482 can receive up to 7 sensors, including thermo-hygrometer (TX-3110B), 
 floating pool and spa thermometer (TX-3107) and soil temperature and moisture (TX-3102)."
@@ -40,8 +42,7 @@ floating pool and spa thermometer (TX-3107) and soil temperature and moisture (T
 
 #include "decoder.h"
 
-#define TX3102_BITLEN      200
-#define TX3102_STARTBYTE   0xAA
+#define TX3102_BITLEN  168
 
 static int ambient_weather_tx3102_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 {
@@ -58,6 +59,8 @@ static int ambient_weather_tx3102_decode(r_device *decoder, bitbuffer_t *bitbuff
     uint16_t c_crc;
     uint8_t dataforcrc[15];
 
+    uint8_t const preamble[] = {0xaa, 0x2d, 0xd4};
+
     /*
      * Early debugging aid to see demodulated bits in buffer and
      * to determine if your limit settings are matched and firing
@@ -71,38 +74,45 @@ static int ambient_weather_tx3102_decode(r_device *decoder, bitbuffer_t *bitbuff
         }
 
     b = bitbuffer->bb[0];
-
-    if (b[0] != TX3102_STARTBYTE) {
-        return 0;
+    unsigned start_pos = bitbuffer_search(bitbuffer, 0, 0, preamble, 24);
+    if (start_pos == bitbuffer->bits_per_row[0]) {
+    return DECODE_ABORT_EARLY;
+    }
+    if (decoder->verbose) {
+        fprintf(stderr, "%s: TX-3102 detected, buffer is %d bits length\n", __func__, bitbuffer->bits_per_row[0]);
     }
 
-    if (b[5] != 0x2D && b[6] != 0xD4) { // check for codeword 2dd4
-	return 0;
-    }
     if (bitbuffer->bits_per_row[0] < TX3102_BITLEN) {
         return DECODE_ABORT_LENGTH;
     }
-    r_crc = (b[7] << 8) | b[8];
+    
+    bitbuffer_extract_bytes(bitbuffer, 0, start_pos + 24, b, 18 * 8);
+
+    if (b[2]!= 0x18) { //check of family code of 0x18
+    return 0;
+    }
+
+    r_crc = (b[0] << 8) | b[1];
     //there has to be a better way to do this
-    dataforcrc[0] = b[9];
-    dataforcrc[1] = b[10];
-    dataforcrc[2] = b[11];
-    dataforcrc[3] = b[12];
-    dataforcrc[4] = b[13];
-    dataforcrc[5] = b[14];
-    dataforcrc[6] = b[15];
-    dataforcrc[7] = b[16];
-    dataforcrc[8] = b[17];
-    dataforcrc[9] = b[18];
-    dataforcrc[10] = b[19];
-    dataforcrc[11] = b[20];
-    dataforcrc[12] = b[21];
-    dataforcrc[13] = b[22];
-    dataforcrc[14] = b[23];
+    dataforcrc[0] = b[2];
+    dataforcrc[1] = b[3];
+    dataforcrc[2] = b[4];
+    dataforcrc[3] = b[5];
+    dataforcrc[4] = b[6];
+    dataforcrc[5] = b[7];
+    dataforcrc[6] = b[8];
+    dataforcrc[7] = b[9];
+    dataforcrc[8] = b[10];
+    dataforcrc[9] = b[11];
+    dataforcrc[10] = b[12];
+    dataforcrc[11] = b[13];
+    dataforcrc[12] = b[14];
+    dataforcrc[13] = b[15];
+    dataforcrc[14] = b[16];
 
     c_crc = crc16(dataforcrc, 15, 0x1021, 0x0000);
 
-    uint8_t c_sum = (add_bytes(dataforcrc, 15) ^ 0xFF) - b[24];
+    uint8_t c_sum = (add_bytes(dataforcrc, 15) ^ 0xFF) - b[17];
     if (c_sum) {
        return DECODE_FAIL_MIC;
     }
@@ -112,22 +122,22 @@ static int ambient_weather_tx3102_decode(r_device *decoder, bitbuffer_t *bitbuff
     }
 
     sprintf(value, "%02x%02x%02x%02x%02x", b[9], b[10], b[11], b[12], b[13]); // unknown data, maybe device type and unique id?
-    sensor_id = (b[13] & 0xF);
+    sensor_id = (b[6] & 0xF);
 
     if (sensor_id > 7) { // channel can only be 1 to 7
 	return DECODE_FAIL_SANITY;
     }
 
-    battery_ok = ((b[20] & 0x02) >> 1);
+    battery_ok = ((b[13] & 0x02) >> 1);
 
-    sign = ((b[20] & 0x08) >> 3);
+    sign = ((b[13] & 0x08) >> 3);
     if (sign == 1) { // negative temperature
-    	temperature = (b[18] & 0xF) * -10 + ((b[19] >> 4) & 0xF) * -1 + (b[19] & 0xF) * -0.1;
+    	temperature = ((b[19] >> 4) & 0xF) * -1 + (b[19] & 0xF) * -0.1;
     }
     else {
-    	temperature = ((b[19] >> 4) & 0xF) * 10 + (b[19] & 0xF) + ((b[20] >> 4) & 0xF) * 0.1;
+    	temperature = ((b[12] >> 4) & 0xF) * 10 + (b[12] & 0xF) + ((b[13] >> 4) & 0xF) * 0.1;
     }
-    moisture = moisture_array[(((b[21] >> 4) & 0xF) * 10 + (b[21] & 0xF)) - 1];
+    moisture = moisture_array[(((b[14] >> 4) & 0xF) * 10 + (b[14] & 0xF)) - 1];
 
     /* clang-format off */
     data = data_make(
@@ -162,7 +172,7 @@ r_device ambient_weather_tx3102 = {
         .modulation  = FSK_PULSE_PCM,
         .short_width = 130,  
         .long_width  = 130,  
-        .reset_limit = 3000,
+        .reset_limit = 7000,
         .decode_fn   = &ambient_weather_tx3102_decode,
         .disabled    = 0,
         .fields      = output_fields,
